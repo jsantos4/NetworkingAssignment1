92874f404e851ef1861fc47a29e9b58c5d9d6c2d
jsantos4 <jsantos4@oswego.edu>
2019-02-27 19:48:14 -0500

    Testing part 3

--- src/main/java/Server.java
+++ src/main/java/Server.java
@@ -52,6 +52,63 @@ public class Server {
 
     }
 
+    public void recieveCombos() {
+        try {
+            for (int i = 0; i < 1024; ++i) {
+                socket = serverSocket.accept();
+                DataInputStream is = new DataInputStream(socket.getInputStream());
+                byte[] bytes = new byte[1024];
+                is.readFully(bytes);
+                DataOutputStream os = new DataOutputStream(socket.getOutputStream());
+                socket.close();
+                os.write(new byte[1]);
+            }
+            for (int j = 0; j < 2048; ++j) {
+                socket = serverSocket.accept();
+                DataInputStream is = new DataInputStream(socket.getInputStream());
+                byte[] bytes = new byte[512];
+                is.readFully(bytes);
+                DataOutputStream os = new DataOutputStream(socket.getOutputStream());
+                os.write(new byte[1]);
+                socket.close();
+            }
+            for (int k = 0; k < 4096; ++k) {
+                socket = serverSocket.accept();
+                DataInputStream is = new DataInputStream(socket.getInputStream());
+                byte[] bytes = new byte[256];
+                is.readFully(bytes);
+                DataOutputStream os = new DataOutputStream(socket.getOutputStream());
+                os.write(new byte[1]);
+                socket.close();
+            }
+            for (int l = 0; l < 1024; ++l) {
+
+                byte[] bytes = new byte[1024];
+                DatagramPacket packet = new DatagramPacket(bytes, 1024);
+                udpSocket.receive(packet);
+                packet = new DatagramPacket(bytes, 1, packet.getAddress(), packet.getPort());
+                udpSocket.send(packet);
+            }
+            for (int m = 0; m < 2048; ++m) {
+
+                byte[] bytes = new byte[512];
+                DatagramPacket packet = new DatagramPacket(bytes, 512);
+                udpSocket.receive(packet);
+                packet = new DatagramPacket(bytes, 1, packet.getAddress(), packet.getPort());
+                udpSocket.send(packet);
+            }
+            for (int n = 0; n < 4096; ++n) {
+                byte[] bytes = new byte[256];
+                DatagramPacket packet = new DatagramPacket(bytes, 256);
+                udpSocket.receive(packet);
+                packet = new DatagramPacket(bytes, 1, packet.getAddress(), packet.getPort());
+                udpSocket.send(packet);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
     public void getPort() {
         System.out.println("TCP Port: " + serverSocket.getLocalPort());
         System.out.println("UDP Port: " + udpSocket.getLocalPort());
